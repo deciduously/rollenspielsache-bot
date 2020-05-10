ee1ce6b8a14d1316234ac2a01b75b64eaa9e3541
Ben Lovy <ben@deciduously.com>
2020-05-10 02:35:13 -0400

    Implement roll command (#2)

--- lib/rollenspielsache/bot.rb
+++ lib/rollenspielsache/bot.rb
@@ -1,8 +1,24 @@
-require "rollenspielsache/bot/version"
+# frozen_string_literal: true
+
+require_relative 'bot/version'
+require 'librollenspielsache'
 
 module Rollenspielsache
   module Bot
     class Error < StandardError; end
-    # Your code goes here...
+    # Rolls from strings
+    class StringRoller
+      def initialize(input_str)
+        @roll = Rollenspielsache::Dice::Roll.from_str(input_str)
+      end
+
+      def base
+        @roll.base
+      end
+
+      def execute
+        @roll.execute
+      end
+    end
   end
 end
